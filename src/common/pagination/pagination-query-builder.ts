daa67cfe55f7f58bfdb168b80d8e722f9f22c1f7
Santiago <sberoch@gmail.com>
2021-10-24 17:56:44 -0300

    default limit up

--- src/common/pagination/pagination-query-builder.ts
+++ src/common/pagination/pagination-query-builder.ts
@@ -4,7 +4,7 @@ import { PaginationParams } from './pagination-params';
 export const buildPaginationQuery = (params: PaginationParams) => {
   const paginationOptions = {
     page: params.page ? parseInt(params.page.toString()) : 1,
-    limit: params.limit ? parseInt(params.limit.toString()) : 1,
+    limit: params.limit ? parseInt(params.limit.toString()) : 2000,
   };
   const orderOptions = getOrder(params.order ? params.order : 'id:asc');
   return {
